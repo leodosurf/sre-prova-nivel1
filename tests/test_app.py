f67f1bbd9bff493169bf74ead4ed6fbce0656803
root <root@ubuntu.0.2.15>
2026-03-13 12:06:03 -0300

    prova-sre1-7

--- tests/test_app.py
+++ tests/test_app.py
@@ -27,10 +27,12 @@ def test_health_endpoint(client):
 
 def test_request_counter(client):
     """Testa se o contador de requisições funciona"""
-    response1 = client.get('/')
+    response1 = client.get('/metrics')
     count1 = response1.get_json()['total_requests']
-    
-    response2 = client.get('/')
+
+    response = client.get('/')
+
+    response2 = client.get('/metrics')
     count2 = response2.get_json()['total_requests']
     
     assert count2 > count1
